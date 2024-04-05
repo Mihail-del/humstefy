124a441c15cceafda80d1f4bb15badfc41993b89
Mihail-del <71522092+Mihail-del@users.noreply.github.com>
2024-04-05 21:53:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// const targetElement = document.getElementById('HandW-info');
+
+// // Create a new Intersection Observer
+// const observer = new IntersectionObserver((entries, observer) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       // When the target element enters the viewport, change opacity to 1
+//       entry.target.style.opacity = 1;
+//     } else {
+//       // When the target element exits the viewport, change opacity back to 0
+//       entry.target.style.opacity = 0;
+//     }
+//   });
+// });
+
+// // Observe the target element
+// observer.observe(targetElement);
